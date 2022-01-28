7205a947875968ead711f738b625babf13bd2efe
Dirtarih <bastricov83@gmail.com>
2022-01-28 11:35:24 +0500

    Кейс 2.3
    Кейс 2.3

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_course_2/case-2-2-pizza.dart';
+import 'package:pizza_course_2/case-2-3-navigation.dart';
+
+
+void main() {
+
+  runApp(const NavigationDrawerScreen());
+}
